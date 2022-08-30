1b9988d164f53fbe49f8fb1a1bee1c4667de5b01
Narumi Abe <smile.narukkory@gmail.com>
2022-08-30 07:46:14 +0000

    [Add]グループメンバーにイベントメール送信の機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   has_many :group_users
   has_many :groups, through: :group_users
 
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
 
   resources :groups do
     get "join" => "groups#join"
+    resources :event_notices, only: [:new, :create]
+    get "event_notices" => "event_notices#sent"
   end
 
   get 'chat/:id', to: 'chats#show', as: 'chat'
